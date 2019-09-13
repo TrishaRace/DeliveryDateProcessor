726b2ff94e7523c74b778828374c8930bb356d52
Patricia Carrera Fernández <patricia.carrera.93@gmail.com>
2019-09-13 11:35:53 +0200

    colors, import libraries,update adapter

--- app/src/main/java/com/carrerap/deliverynotesprocessor/DeliveryNote/DeliveryNoteAdapter.kt
+++ app/src/main/java/com/carrerap/deliverynotesprocessor/DeliveryNote/DeliveryNoteAdapter.kt
@@ -5,8 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.carrerap.deliverynotesprocessor.R
+import kotlinx.android.synthetic.main.item_delivery_note.view.*
 
-class DeliveryNoteAdapter(var deliveryNoteList : ArrayList<DeliveryNote>, clickListener: (DeliveryNote)->Unit) : RecyclerView.Adapter<DeliveryNoteAdapter.ViewHolder>(){
+class DeliveryNoteAdapter(var deliveryNoteList : ArrayList<DeliveryNote>, val clickListener: (DeliveryNote)->Unit) : RecyclerView.Adapter<DeliveryNoteAdapter.ViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_delivery_note, parent, false))
     }
@@ -19,17 +20,13 @@ class DeliveryNoteAdapter(var deliveryNoteList : ArrayList<DeliveryNote>, clickL
         holder.bindItems(deliveryNoteList[position],clickListener)
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_delivery_note, parent, false))
-        }
-
-        override fun getItemCount(): Int {
-            return deliveryNoteList.size
-        }
-
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            holder.bindItems(deliveryNoteList[position],clickListener)
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bindItems(deliveryNote: DeliveryNote,clickListener:(DeliveryNote)->Unit ){
+            itemView.tvDeliveryNoteCode.text = deliveryNote.code.toString()
+            itemView.tvDeliveryNoteDescription.text = deliveryNote.description
+            itemView.tvDeliveryNoteQuantity.text = deliveryNote.quantity.toString()
+            itemView.tvDeliveryNotePrice.text = deliveryNote.price.toString()
         }
     }
+    
 }
